763b0c1e957054c7276e28252151050c7580fbe0
Jay <j19950310@gmail.com>
2023-04-20 13:38:16 +0800

    To env

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-token:
+token(過期):
 MTA3NTY2NDUzNDUwNjg0MDA3NA.GFc0RY.l3E2P73bb-Sn0vpgzVNIZ8OgT-oo0fqktdut8c
+
+https://discord.com/oauth2/authorize?client_id=1075664534506840074&permissions=274878155840&scope=bot
--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 import { REST } from "@discordjs/rest";
 import { API } from "@discordjs/core";
+import { Configuration, OpenAIApi } from "openai";
 
-const token =
-  "MTA3NTY2NDUzNDUwNjg0MDA3NA.GFc0RY.l3E2P73bb-Sn0vpgzVNIZ8OgT-oo0fqktdut8c";
-const channelId = "1098464995454492714";
+// openai
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// discord
+const token = process.env.DISCORD_TOKEN;
+const channelId = process.env.DISCORD_CHANNEL_ID;
 
 const rest = new REST({ version: "10" }).setToken(token);
-
-// Pass into API
 const api = new API(rest);
+
+// Start
+let questionString = "";
+try {
+  const completion = await openai.createChatCompletion({
+    model: "gpt-4",
+    temperature: 0.9,
+    messages: [
+      { role: "system", content: "You are an annoying contact person." },
+      {
+        role: "user",
+        content:
+          "隨機給我點10字以內的難題，不用限制我時間跟做法，要求個圖片視覺的意象。",
+      },
+      { role: "assistant", content: "我想要一個圖片，裡面" },
+    ],
+  });
+  questionString = completion.data.choices[0].message.content;
+} catch (error) {
+  console.error(error);
+}
 
 // createMessage
 const message = await api.channels.createMessage(channelId, {
-  content: "Hello World!",
+  content: "阿對了，我今天有個需求，我想要一個意境圖，裡面: " + questionString,
   tts: false,
 });
